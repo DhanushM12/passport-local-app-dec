56b8f365a0fd4f5315f99fc0a53fe683beff258c
DhanushM12 <dhanushmukhi@gmail.com>
2022-08-21 14:06:23 +0530

    added setup and ui

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+const express = require('express');
+const port = 8000;
+const app = express();
+
+
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error in running the server: ${err}`);
+    }
+    console.log(`Server is up and running on port : ${port}`)
+})
